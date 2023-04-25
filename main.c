2ffa8923b9c542a8fc5f4da9d33dc16ce52cc773
legitcoder04 <zeroualoussama2@gmail.com>
2023-04-25 16:59:39 -0700

    update

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-      _printf("Hello, %s! I have %c%c%c%c %d%% of my tasks done today.\n", "John", 'C', 'o', 'd', 'e', 75);
-    _printf("Hello, %s! I have %c%c%c%c %d%% of my tasks done today. Signed integers: %d, %i\n", "John", 'C', 'o', 'd', 'e', 75, -123, 456);
-    _printf("98 in binary: %b\n", 98);
-    return 0;
+    _printf("%b\n", 98);
+    return (0);
 }
-
