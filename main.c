c29737202a98214fef0ab01be5e5fd7e6bfdf811
legitcoder04 <zeroualoussama2@gmail.com>
2023-04-25 16:48:29 -0700

    update

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-int main(void) {
+int main(void)
+{
     _printf("Hello, %s! I have %c%c%c%c %d%% of my tasks done today.\n", "John", 'C', 'o', 'd', 'e', 75);
     return 0;
 }
--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 int _printf(const char *format, ...);
+void print_number(int num);
 
 #endif /* MAIN_H */
 
