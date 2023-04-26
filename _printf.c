bea35540d571d86560a74007e3bd3fcf8d0aecfb
abde109 <abderrahimkhadri2@gmail.com>
2023-04-25 17:09:58 -0700

    update

--- _printf.c
+++ _printf.c
@@ -20,12 +20,34 @@ int _printf(const char *format, ...)
         p++;
 
         switch (*p)
-        {
-            case 'b':
+	{
+
+            case 'u':
             {
-                unsigned int b = va_arg(args, unsigned int);
-                print_binary(b);
-                count += count_binary_digits(b);
+                unsigned int u = va_arg(args, unsigned int);
+                print_unsigned(u);
+                count += count_digits(u);
+                break;
+            }
+            case 'o':
+            {
+                unsigned int o = va_arg(args, unsigned int);
+                print_octal(o);
+                count += count_digits(o);
+                break;
+            }
+            case 'x':
+            {
+                unsigned int x = va_arg(args, unsigned int);
+                print_hex(x, 0);
+                count += count_digits(x);
+                break;
+            }
+            case 'X':
+            {
+                unsigned int x = va_arg(args, unsigned int);
+                print_hex(x, 1);
+                count += count_digits(x);
                 break;
             }
             default:
@@ -42,8 +64,7 @@ int _printf(const char *format, ...)
     return count;
 }
 
-
-void print_binary(unsigned int num)
+void print_unsigned(unsigned int num)
 {
     if (num == 0)
     {
@@ -51,23 +72,41 @@ void print_binary(unsigned int num)
         return;
     }
 
-    if (num / 2)
-        print_binary(num / 2);
+    if (num / 10)
+        print_unsigned(num / 10);
 
-    putchar((num % 2) + '0');
+    putchar((num % 10) + '0');
 }
 
-int count_binary_digits(unsigned int num)
+void print_octal(unsigned int num)
 {
-    int count = 0;
     if (num == 0)
-        return 1;
-
-    while (num != 0)
     {
-        num /= 2;
-        count++;
+        putchar('0');
+        return;
     }
-    return count;
+
+    if (num / 8)
+        print_octal(num / 8);
+
+    putchar((num % 8) + '0');
+}
+
+void print_hex(unsigned int num, int uppercase)
+{
+    char *hex_digits;
+
+    if (num == 0)
+    {
+        putchar('0');
+        return;
+    }
+
+    hex_digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+
+    if (num / 16)
+        print_hex(num / 16, uppercase);
+
+    putchar(hex_digits[num % 16]);
 }
 
--- main.h
+++ main.h
@@ -8,6 +8,9 @@ int _printf(const char *format, ...);
 void print_number(int num);
 void print_binary(unsigned int num);
 int count_binary_digits(unsigned int num);
+void print_unsigned(unsigned int num);
+void print_octal(unsigned int num);
+void print_hex(unsigned int num, int uppercase);
 
 #endif /* MAIN_H */
 
