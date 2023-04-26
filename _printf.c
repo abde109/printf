e20434b5e9886cc1052cceb87f5e46489a19c54e
abde109 <abderrahimkhadri2@gmail.com>
2023-04-25 18:20:44 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,7 +1,11 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
+
+void print_unsigned(unsigned int u);
+void print_octal(unsigned int o);
+void print_hex(unsigned int x, int uppercase);
+int count_digits(unsigned int num);
+int count_octal_digits(unsigned int num);
+int count_hex_digits(unsigned int num);
 
 int _printf(const char *format, ...)
 {
@@ -35,21 +39,21 @@ int _printf(const char *format, ...)
             {
                 unsigned int o = va_arg(args, unsigned int);
                 print_octal(o);
-                count += count_digits(o);
+                count += count_octal_digits(o);
                 break;
             }
             case 'x':
             {
                 unsigned int x = va_arg(args, unsigned int);
                 print_hex(x, 0);
-                count += count_digits(x);
+                count += count_hex_digits(x);
                 break;
             }
             case 'X':
             {
                 unsigned int x = va_arg(args, unsigned int);
                 print_hex(x, 1);
-                count += count_digits(x);
+                count += count_hex_digits(x);
                 break;
             }
             default:
@@ -68,35 +72,68 @@ int _printf(const char *format, ...)
 
 void print_unsigned(unsigned int u)
 {
-    if (u / 10 != 0)
+    if (u >= 10)
+    {
         print_unsigned(u / 10);
-    putchar('0' + (u % 10));
+    }
+    putchar('0' + u % 10);
 }
 
 void print_octal(unsigned int o)
 {
-    if (o / 8 != 0)
+    if (o >= 8)
+    {
         print_octal(o / 8);
-    putchar('0' + (o % 8));
+    }
+    putchar('0' + o % 8);
 }
 
 void print_hex(unsigned int x, int uppercase)
 {
-    char c;
-    if (x / 16 != 0)
+    if (x >= 16)
+    {
         print_hex(x / 16, uppercase);
-    c = (x % 16) + (x % 16 < 10 ? '0' : (uppercase ? 'A' : 'a') - 10);
-    putchar(c);
+    }
+    if (x % 16 < 10)
+    {
+        putchar('0' + x % 16);
+    }
+    else
+    {
+        putchar((uppercase ? 'A' : 'a') + x % 16 - 10);
+    }
 }
 
 int count_digits(unsigned int num)
 {
     int count = 0;
-    while (num > 0)
+    do
     {
         count++;
         num /= 10;
-    }
+    } while (num > 0);
+    return count;
+}
+
+int count_octal_digits(unsigned int num)
+{
+    int count = 0;
+    do
+    {
+        count++;
+        num /= 8;
+    } while (num > 0);
+    return count;
+}
+
+int count_hex_digits(unsigned int num)
+{
+    int count = 0;
+    do
+    {
+        count++;
+        num /= 16;
+    } while (num > 0);
     return count;
 }
 
