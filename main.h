7adfb925658d662372590fa8b010efc9a82565f4
abde109 <abderrahimkhadri2@gmail.com>
2023-04-25 18:36:10 -0700

    update

--- main.h
+++ main.h
@@ -1,20 +1,18 @@
+/* main.h */
 #ifndef MAIN_H
 #define MAIN_H
-
 #define BUFFER_SIZE 1024
 
 #include <stdarg.h>
-#include <stdio.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
-void print_number(int num);
-void print_binary(unsigned int num);
-int count_binary_digits(unsigned int num);
-void print_unsigned(unsigned int num);
-void print_octal(unsigned int num);
-void print_hex(unsigned int num, int uppercase);
+void print_unsigned(unsigned int u, char *buffer, int *buffer_index);
+void print_octal(unsigned int o, char *buffer, int *buffer_index);
+void print_hex(unsigned int x, int uppercase, char *buffer, int *buffer_index);
 int count_digits(unsigned int num);
-int count_octal_digits(unsigned int num);
-int count_hex_digits(unsigned int num);
+void print_binary(unsigned int b, char *buffer, int *buffer_index);
+void flush_buffer(char *buffer, int *buffer_index);
+
 #endif /* MAIN_H */
 
